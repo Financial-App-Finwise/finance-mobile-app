65c00e288f530356e0d40581fcd140d11b08e813
Samedy <phinsamedy@gmail.com>
2024-01-19 15:09:08 +0700

    finished bar chart, added pie chart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,8 +2,10 @@ import 'package:finwise/core/constants/color_constant.dart';
 import 'package:finwise/core/constants/home_text_style_constant.dart';
 import 'package:finwise/modules/auth/screens/sign_in_screen.dart';
 import 'package:finwise/modules/budget_plan/screens/budget_plan_screen.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_flutter/icons_flutter.dart';
+import 'package:mobkit_dashed_border/mobkit_dashed_border.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -31,15 +33,21 @@ class _HomeScreenState extends State<HomeScreen> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              _buildProfile(), // almost
-              _buildFinance(), // almost
-              _buildMainFeatures(),
-              _buildSpendingIncome(),
-              _buildTopSpending(),
-              _buildBudgetPlan(),
-              _buildTotalSpend(),
-              _buildTotalEarn(),
-              _buildUpcomingBill(),
+              ListView(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                children: [
+                  _buildProfile(), // almost
+                  _buildFinance(), // almost
+                  _buildMainFeatures(), // almost
+                  _buildSpendingIncome(),
+                  _buildTopSpending(),
+                  _buildBudgetPlan(),
+                  _buildTotalSpend(),
+                  _buildTotalEarn(),
+                  _buildUpcomingBill(),
+                ],
+              ),
             ],
           ),
         ),
@@ -79,8 +87,10 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           InkWell(
             onTap: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => const SignInScreen()));
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SignInScreen()));
             },
             child: const Icon(Icons.notifications_active_outlined),
           ),
@@ -119,13 +129,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           const SizedBox(height: 12),
           // build content
-          Container(
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-            ),
+          _roundedContainer(
             child: Column(
               children: [
                 // Total balance
@@ -149,7 +153,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 Container(
                   // color: Colors.pink,
                   padding: const EdgeInsets.symmetric(vertical: 14),
-                  child: Divider(color: Colors.grey[300]),
+                  child: Divider(color: Color(0xfff2f2f2)),
                 ),
                 // Income and Expense
                 IntrinsicHeight(
@@ -168,7 +172,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                       Container(
                         padding: const EdgeInsets.symmetric(horizontal: 4),
-                        child: VerticalDivider(color: Colors.grey[300]),
+                        child: VerticalDivider(color: Color(0xfff2f2f2)),
                       ),
                       Expanded(
                         child: _buildFinanceItem(
@@ -204,16 +208,9 @@ class _HomeScreenState extends State<HomeScreen> {
         const SizedBox(width: 12),
         Column(
           children: [
-            Text(
-              text,
-              style: HomeTextStyleConstant.medium,
-            ),
-            Text(
-              amount,
-              style: HomeTextStyleConstant.numberFocus(
-                color: color,
-              ),
-            ),
+            Text(text, style: HomeTextStyleConstant.medium),
+            Text(amount,
+                style: HomeTextStyleConstant.numberFocus(color: color)),
           ],
         ),
       ],
@@ -247,17 +244,20 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  // Other Features
+
   Widget _buildMainFeatures() {
     return Container(
       alignment: Alignment.topLeft,
-      color: Colors.red[100],
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          SizedBox(height: 20),
           Text(
             'Other Features',
             style: HomeTextStyleConstant.header,
           ),
+          SizedBox(height: 12),
           SingleChildScrollView(
             physics: const BouncingScrollPhysics(),
             scrollDirection: Axis.horizontal,
@@ -284,10 +284,16 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _buildFeatureItem(
-      {String text = '', String amount = '', void Function()? onPressed}) {
+  Widget _buildFeatureItem({
+    String text = '',
+    String amount = '',
+    void Function()? onPressed,
+  }) {
     return TextButton(
       onPressed: onPressed,
+      style: ButtonStyle(
+        padding: MaterialStateProperty.all(EdgeInsets.zero),
+      ),
       child: Container(
         width: 165.5,
         alignment: Alignment.center,
@@ -324,29 +330,272 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  /////////
+
   Widget _buildSpendingIncome() {
     return Container(
-      color: Colors.orange[100],
+      // color: Colors.green[100],
       alignment: Alignment.topLeft,
-      child: Text(
-        'Spending and Income',
-        style: HomeTextStyleConstant.header,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 20),
+          Text(
+            'Spending and Income',
+            style: HomeTextStyleConstant.header,
+          ),
+          SizedBox(height: 12),
+          _roundedContainer(
+            child: Column(
+              children: [
+                Row(children: [
+                  Expanded(child: SizedBox()),
+                  _buildDurationButton()
+                ]),
+                SizedBox(height: 24),
+                _buildBarChart(),
+                SizedBox(height: 24),
+                _buildLegend(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  Widget _roundedContainer({Widget? child}) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+      ),
+      padding: EdgeInsets.all(16),
+      child: child,
+    );
+  }
+
+  Widget _buildBarChart() {
+    return Container(
+      height: 200,
+      child: BarChart(
+        BarChartData(
+          titlesData: FlTitlesData(
+            rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+            topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+            bottomTitles: AxisTitles(
+              sideTitles: SideTitles(
+                reservedSize: 32,
+                showTitles: true,
+                getTitlesWidget: (value, meta) =>
+                    _barChartBottomTitles(value, meta),
+              ),
+            ),
+            leftTitles: AxisTitles(
+              sideTitles: SideTitles(
+                reservedSize: 48,
+                showTitles: true,
+                interval: 1000,
+                getTitlesWidget: (value, meta) {
+                  return _barChartleftTitles(value, meta);
+                },
+              ),
+            ),
+          ),
+          gridData: FlGridData(
+            getDrawingVerticalLine: (value) {
+              return FlLine(
+                color: Color(0xfff2f2f2),
+                dashArray: [4],
+                strokeWidth: 1,
+              );
+            },
+            getDrawingHorizontalLine: (value) {
+              return FlLine(
+                color: Color(0xfff2f2f2),
+                dashArray: [4],
+                strokeWidth: 1,
+              );
+            },
+            verticalInterval: 0.25,
+            horizontalInterval: 1000,
+          ),
+          borderData: FlBorderData(
+            border: DashedBorder.fromBorderSide(
+              side: BorderSide(color: Color(0xfff2f2f2)),
+              dashLength: 4,
+            ),
+          ),
+          alignment: BarChartAlignment.spaceAround,
+          barGroups: [
+            _buildBarChartGroup(x: 0, income: 6000, expense: 4000),
+            _buildBarChartGroup(x: 1, income: 4000, expense: 2000),
+            _buildBarChartGroup(x: 2, income: 8000, expense: 6000),
+            _buildBarChartGroup(x: 3, income: 7000, expense: 3000),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BarChartGroupData _buildBarChartGroup({
+    required int x,
+    double income = 0,
+    double expense = 0,
+  }) {
+    return BarChartGroupData(x: x, barRods: [
+      _buildBarRod(isIncome: true, toY: income),
+      _buildBarRod(toY: expense),
+    ]);
+  }
+
+  BarChartRodData _buildBarRod({
+    bool isIncome = false,
+    double toY = 0,
+  }) {
+    return BarChartRodData(
+      toY: toY,
+      width: 7,
+      color: isIncome ? ColorConstant.income : ColorConstant.expense,
+      borderRadius: BorderRadius.zero,
+    );
+  }
+
+  Widget _barChartBottomTitles(double value, TitleMeta meta) {
+    final titles = ['Sep 2023', 'Oct 2023', 'Nov 2023', 'Dec 2023'];
+    print(value);
+    return SideTitleWidget(
+      child: Text(titles[value.toInt()],
+          style: HomeTextStyleConstant.barChartBottomTitle),
+      axisSide: meta.axisSide,
+    );
+  }
+
+  Widget _barChartleftTitles(double value, TitleMeta meta) {
+    return SideTitleWidget(
+      child: Text('\$${value.toInt()}',
+          style: HomeTextStyleConstant.barChartLeftTitle),
+      axisSide: meta.axisSide,
+    );
+  }
+
+  Widget _buildLegend() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(child: _buildLegendItem('Income', ColorConstant.income)),
+        Expanded(child: _buildLegendItem('Expense', ColorConstant.expense)),
+      ],
+    );
+  }
+
+  Widget _buildLegendItem(String text, Color color) {
+    return Row(
+      children: [
+        Container(
+          height: 16,
+          width: 16,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+        ),
+        SizedBox(width: 8),
+        Text(text, style: HomeTextStyleConstant.barChartLegend),
+      ],
+    );
+  }
+
+  ///
 
   Widget _buildTopSpending() {
     return Container(
+      color: Colors.blue[100],
       alignment: Alignment.topLeft,
-      child: Text(
-        'Top Spending Categories',
-        style: HomeTextStyleConstant.header,
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Text(
+            'Top Spending Categories',
+            style: HomeTextStyleConstant.header,
+          ),
+          SizedBox(height: 12),
+          _roundedContainer(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text('Text'),
+                    ),
+                    _buildDurationButton(),
+                  ],
+                ),
+                SizedBox(height: 30),
+                _buildPieChart(),
+                SizedBox(height: 30),
+                _buildPieChartLegend(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
+  Widget _buildPieChart() {
+    List values = [40.0, 20.0, 30.0, 10.0];
+    values.sort((a, b) => b.compareTo(a));
+    List opacities = [1.0, 0.75, 0.50, 0.25];
+
+    print(values);
+    return Container(
+      height: 200,
+      child: PieChart(
+        PieChartData(
+          startDegreeOffset: 180,
+          sections: [
+            _buildPieChartItem(value: values[0], opacity: opacities[0]),
+            _buildPieChartItem(value: values[1], opacity: opacities[1]),
+            _buildPieChartItem(value: values[2], opacity: opacities[2]),
+            _buildPieChartItem(value: values[3], opacity: opacities[3]),
+          ],
+        ),
+      ),
+    );
+  }
+
+  PieChartSectionData _buildPieChartItem({
+    double value = 0.0,
+    double opacity = 1.0,
+  }) {
+    return PieChartSectionData(
+      value: value,
+      color: ColorConstant.topSpending.withOpacity(opacity),
+      showTitle: false,
+    );
+  }
+
+  Widget _buildPieChartLegend() {
+    return Row(
+      children: [
+        Text('item1')
+      ],
+    );
+  }
+
+  ////
+  ///
+  ////////
+  ///
+  ///
+  ///
+  ///
+
   Widget _buildBudgetPlan() {
     return Container(
+      color: Colors.pink[200],
       alignment: Alignment.topLeft,
       child: Text(
         'My Budget Plan',
@@ -358,9 +607,14 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget _buildTotalSpend() {
     return Container(
       alignment: Alignment.topLeft,
-      child: Text(
-        'Totally Spent',
-        style: HomeTextStyleConstant.header,
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Text(
+            'Totally Spent',
+            style: HomeTextStyleConstant.header,
+          ),
+        ],
       ),
     );
   }
