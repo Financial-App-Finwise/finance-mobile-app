9841cf6a3806d2c6a944978299f32af24c81e450
Samedy <phinsamedy@gmail.com>
2024-01-23 15:46:15 +0700

    new routes

--- lib/route.dart
+++ lib/route.dart
@@ -1,10 +1,25 @@
-import 'package:finwise/screens/home_screen.dart';
+import 'package:finwise/modules/auth/screens/sign_in_screen.dart';
+import 'package:finwise/modules/auth/screens/sign_up_screen.dart';
+import 'package:finwise/modules/budget_plan/screens/budget_plan_screen.dart';
+import 'package:finwise/modules/home/screens/home_screen.dart';
 import 'package:finwise/screens/index_screen.dart';
 import 'package:finwise/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
+abstract class RouteName {
+  static String splash = '/splash';
+  static String signIn = '/sign_in';
+  static String signUp = '/sign_up';
+  static String index = '/index';
+  static String home ='/home';
+  static String budgetPlan = '/budget_plan';
+}
+
 var appRoutes = {
-  '/splash': (BuildContext context) => SplashScreen(),
-  '/index': (BuildContext context) => IndexScreen(),
-  '/home': (BuildContext context) => HomeScreen(),
+  RouteName.splash: (BuildContext context) => SplashScreen(),
+  RouteName.splash: (BuildContext context) => SignInScreen(),
+  RouteName.splash: (BuildContext context) => SignUpScreen(),
+  RouteName.index: (BuildContext context) => IndexScreen(),
+  RouteName.home: (BuildContext context) => HomeScreen(),
+  RouteName.budgetPlan: (BuildContext context) => BudgetPlanScreen(),
 };
