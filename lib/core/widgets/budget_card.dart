4a898b33e9189e3899e41fc2e3b4f230c14998da
Samedy <phinsamedy@gmail.com>
2024-01-24 10:12:09 +0700

    added budget widgets

---
+++ lib/core/widgets/budget_card.dart
@@ -0,0 +1,202 @@
+import 'package:finwise/core/constants/color_constant.dart';
+import 'package:finwise/core/widgets/custom_progess_bar.dart';
+import 'package:flutter/material.dart';
+
+class BudgetCard extends StatelessWidget {
+  final Widget screen;
+  final String title;
+  final Color color;
+  final int transaction;
+  final int remain;
+  final int total;
+  final int spent;
+
+  const BudgetCard({
+    super.key,
+    required this.screen,
+    this.title = 'title',
+    this.color = Colors.black,
+    this.transaction = 0,
+    this.remain = 0,
+    this.total = 0,
+    this.spent = 0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => screen,
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: ColorConstant.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      color: color, borderRadius: BorderRadius.circular(4)),
+                  child: Icon(
+                    Icons.car_rental_outlined,
+                    color: ColorConstant.white,
+                  ),
+                ),
+                const SizedBox(
+                  width: 11,
+                ),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF000000),
+                  ),
+                ),
+              ],
+            ),
+            const Divider(
+              color: Color(0xFFF2F2F2),
+            ),
+            // _card(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _card() {
+    return Container(
+      decoration: BoxDecoration(
+        color: ColorConstant.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    '$transaction',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF191B29),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 6,
+                  ),
+                  Text(
+                    'transactions',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: ColorConstant.mainText,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    '\$$remain',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF191B29),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 6,
+                  ),
+                  Text(
+                    'left',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: ColorConstant.mainText,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          CustomProgressBar(
+            value: ((spent * 10) / total) / 10,
+            color: color,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    '\$$spent',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF191B29),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 6,
+                  ),
+                  Text(
+                    'spent',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: ColorConstant.mainText,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  const Text(
+                    'out of',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF333652),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 6,
+                  ),
+                  Text(
+                    '\$$total',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF191B29),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/core/widgets/budget_overview.dart
@@ -0,0 +1,214 @@
+import 'package:finwise/core/constants/color_constant.dart';
+import 'package:finwise/core/constants/icon_constant.dart';
+import 'package:flutter/material.dart';
+
+class BudgetOverview extends StatelessWidget {
+  final int totalBudget;
+  final int available;
+  final int spend;
+  final int overBudget;
+  final int plannedBudget;
+
+  const BudgetOverview({
+    super.key,
+    required this.totalBudget,
+    required this.available,
+    required this.spend,
+    required this.overBudget,
+    required this.plannedBudget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Total budgets
+        // _totalBudget(),
+
+        // Gap
+        const SizedBox(
+          height: 16,
+        ),
+
+        // Budget Overview Card
+        _budgetOverviewCard(),
+      ],
+    );
+  }
+
+  // Total budget
+  Widget _totalBudget() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+      decoration: BoxDecoration(
+          color: const Color(0xFFFFFFFF),
+          borderRadius: BorderRadius.circular(12)),
+      child: Column(children: [
+        Row(
+          children: [
+            SizedBox(
+              height: 36,
+              width: 36,
+              child: IconConstant.myBudget(
+                color: ColorConstant.secondary,
+              ),
+            ),
+
+            const SizedBox(
+              width: 12,
+            ),
+            // Text
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Total budgets',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF333652),
+                  ),
+                ),
+                const SizedBox(
+                  height: 1,
+                ),
+                Text(
+                  '$totalBudget',
+                  style: const TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF0B8AAF),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ]),
+    );
+  }
+
+  // Budget overview card
+  Widget _budgetOverviewCard() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: const Color(0xFFFFFFFF),
+          border: Border.all(color: Color(0xffE9EAF1)),
+          borderRadius: BorderRadius.circular(12)),
+      child: Column(
+        children: [
+          IntrinsicHeight(
+            child: Row(
+              children: [
+                Expanded(
+                  child: _budgetOverviewCardSection(
+                    IconConstant.available,
+                    'Available',
+                    available,
+                    const Color(0xFF35D2A5),
+                  ),
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                const VerticalDivider(
+                  color: Color(0xFFF2F2F2),
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                Expanded(
+                  child: _budgetOverviewCardSection(
+                    IconConstant.spend,
+                    'Spend',
+                    spend,
+                    const Color(0xFFEE5353),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(
+            color: Color(0xFFF2F2F2),
+          ),
+          IntrinsicHeight(
+            child: Row(
+              children: [
+                Expanded(
+                  child: _budgetOverviewCardSection(
+                    IconConstant.warning,
+                    'Over Budget',
+                    overBudget,
+                    const Color(0xFFEF6007),
+                  ),
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                const VerticalDivider(
+                  color: Color(0xFFF2F2F2),
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                Expanded(
+                  child: _budgetOverviewCardSection(
+                    IconConstant.budgetPlan,
+                    'Planned Budget',
+                    plannedBudget,
+                    const Color(0xFF0B8AAF),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Budget overview section
+  Widget _budgetOverviewCardSection(
+      Widget icon, String category, int amount, Color textColor) {
+    return Container(
+      alignment: Alignment.center,
+      child: Row(
+        children: [
+          icon,
+          const SizedBox(
+            width: 12,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  category,
+                  maxLines: 2,
+                  softWrap: true,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF333652),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Text(
+                  '\$$amount',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                    color: textColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
