2bfe75722bf0c919c95bce3d7120978a63ce572b
Samedy <phinsamedy@gmail.com>
2024-01-26 11:24:11 +0700

    added logout button in profile

---
+++ lib/modules/user/profile_screen.dart
@@ -0,0 +1,31 @@
+import 'package:finwise/modules/auth/stores/auth_store.dart';
+import 'package:finwise/screens/unimplemented_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: UnimplementedScreen()),
+          ElevatedButton(
+            onPressed: () {
+              context.read<AuthStore>().signOut();
+            },
+            child: Text('Log out'),
+          ),
+          SizedBox(height: 48),
+        ],
+      ),
+    );
+  }
+}
