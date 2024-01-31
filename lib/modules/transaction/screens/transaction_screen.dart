e432b2e841bd78debc1a2ab704f5949fb57006e6
Samedy <phinsamedy@gmail.com>
2024-01-31 18:55:52 +0700

    added transaction

---
+++ lib/modules/transaction/screens/transaction_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TransactionScreen extends StatefulWidget {
+  const TransactionScreen({super.key});
+
+  @override
+  State<TransactionScreen> createState() => _TransactionScreenState();
+}
+
+class _TransactionScreenState extends State<TransactionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Text('Hello World!');
+  }
+}
