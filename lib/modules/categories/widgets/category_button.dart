1ae297f63406784ab0c72bc19af613f9472a1942
OukVathanak <oukk.vathanak@gmail.com>
2024-02-13 18:31:47 +0700

    UPDATE category id

--- lib/modules/categories/widgets/category_button.dart
+++ lib/modules/categories/widgets/category_button.dart
@@ -1,4 +1,6 @@
 import 'package:finwise/core/constants/color_constant.dart';
+import 'package:finwise/core/constants/svg_name_constant.dart';
+import 'package:finwise/core/helpers/icon_helper.dart';
 import 'package:finwise/modules/categories/models/categories_model.dart';
 import 'package:finwise/modules/categories/screens/category_screen.dart';
 import 'package:flutter/material.dart';
@@ -28,47 +30,102 @@ class _CategoryButtonState extends State<CategoryButton> {
                     CategoryScreen(setCategory: widget.setCategory),
               ),
             ),
-        child: Container(
-          padding: const EdgeInsets.symmetric(
-            vertical: 16,
-            horizontal: 20,
-          ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: ColorConstant.white,
-          ),
-          child: Row(
-            children: [
-              Container(
-                width: 36,
-                height: 36,
-                decoration: BoxDecoration(
-                  color: ColorConstant.topSpending,
-                  borderRadius: BorderRadius.circular(50),
-                ),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16,
+                horizontal: 20,
               ),
-              const SizedBox(
-                width: 8,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: ColorConstant.white,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Row(
                 children: [
-                  Text('Category'),
-                  Text(
-                    widget.category?.name == 'no name'
-                        ? 'Select one of the category'
-                        : widget.category!.name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 12,
-                      letterSpacing: 0.75,
-                      color: ColorConstant.mainText,
-                    ),
+                  widget.category?.name == 'no name'
+                      ? _unpickCategoryIcon()
+                      : _pickedCategoryIcon(),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Category',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          letterSpacing: 0.75,
+                          color: ColorConstant.mainText,
+                        ),
+                      ),
+                      Text(
+                        widget.category?.name == 'no name'
+                            ? 'Select one of the category'
+                            : widget.category!.name,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                          letterSpacing: 0.75,
+                          color: widget.category?.name == 'no name'
+                              ? const Color(0xFF656B9F)
+                              : ColorConstant.mainText,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const Text(
+              'Spending in this category and sub-category will be accounted to this plan.',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 12,
+                letterSpacing: 0.5,
+                color: ColorConstant.mainText,
+              ),
+            )
+          ],
         ));
+  }
+
+  Widget _unpickCategoryIcon() {
+    return Container(
+      width: 36,
+      height: 36,
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(6),
+      decoration: BoxDecoration(
+        color: const Color(0xFFD3D5E4),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: IconHelper.getSVG(
+        SVGName.expense,
+        color: ColorConstant.white,
+      ),
+    );
+  }
+
+  Widget _pickedCategoryIcon() {
+    return Container(
+      width: 36,
+      height: 36,
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(6),
+      decoration: BoxDecoration(
+        color: ColorConstant.expenseIcon,
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: IconHelper.getSVG(
+        SVGName.schoolBus,
+        color: ColorConstant.white,
+      ),
+    );
   }
 }
