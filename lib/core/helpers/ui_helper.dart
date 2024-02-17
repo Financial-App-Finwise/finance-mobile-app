43fd313b691be873125f0c05ee34ea646018b538
Samedy1 <samedyphin1@gmail.com>
2024-02-17 14:47:12 +0700

    helpers

--- lib/core/helpers/ui_helper.dart
+++ lib/core/helpers/ui_helper.dart
@@ -1,10 +1,28 @@
 import 'package:intl/intl.dart';
 
-class UIHelper {
+abstract class UIHelper {
   static getFormattedDate(String date) {
     if (DateTime.tryParse(date) != null) {
       DateTime parsedDate = DateTime.parse(date);
       String formattedDate = DateFormat('dd MMMM, yyyy').format(parsedDate);
+      return formattedDate;
+    }
+    return date;
+  }
+
+  static getInputDate(String date) {
+    if (DateTime.tryParse(date) != null) {
+      DateTime parsedDate = DateTime.parse(date);
+      String formattedDate = DateFormat('dd/MM/yyyy').format(parsedDate);
+      return formattedDate;
+    }
+    return date;
+  }
+
+  static getYYYYMMDD(String date) {
+    if (DateTime.tryParse(date) != null) {
+      DateTime parsedDate = DateTime.parse(date);
+      String formattedDate = DateFormat('yyyy-MM-dd').format(parsedDate);
       return formattedDate;
     }
     return date;
